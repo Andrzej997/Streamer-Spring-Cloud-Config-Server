f1a7ed5ede9063690a6dc0ebb8b74db112cbf0d9
m.sojka <mateusz.sojka@asseco.pl>
2018-04-24 23:00:17 +0200

    database fixed

--- database-config/usersdb/Usersdb_DDL.sql
+++ database-config/usersdb/Usersdb_DDL.sql
@@ -50,7 +50,7 @@ create extension postgres_fdw;
 
 INSERT INTO USERS_SCHEMA.USERS
 (USER_ID, USER_NAME, PASSWORD, NAME, SURNAME, NATIONALITY, EMAIL, account_expiration_date, account_locked, password_expiration_date)
-VALUES (nextval('DEFAULTDBSEQ'), 'admin', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',
+VALUES (nextval('DEFAULTDBSEQ'), 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
 'admin', 'admin', 'PL', 'admin@admin', current_date + 120, false, current_date + 120);
 
 INSERT INTO USERS_SCHEMA.AUTHORITY
